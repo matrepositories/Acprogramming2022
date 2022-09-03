36547335fcdfe1b4518bbe7a14740e15c28d9394
matrepositories <112382537+matrepositories@users.noreply.github.com>
2022-09-03 12:34:16 +0300

    Delete A_cprogramming/M08/M08 directory

--- A_cprogramming/M08/M08/T2-str-split/src/stringsplit.h
+++
@@ -1,8 +0,0 @@
-#ifndef AALTO_STRINGSPLIT_H
-#define AALTO_STRINGSPLIT_H
-
-char** split_string(const char *str, const char* split);
-void print_split_string(char **split_string);
-void free_split_string(char **split_string);
-
-#endif
